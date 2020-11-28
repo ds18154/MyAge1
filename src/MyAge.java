9e0b5320e00bb6d329b625005f595d8da3302263
ds18154 <ds18154@go.uop.gr>
2020-11-28 20:12:10 +0200

    Initial commit

---
+++ src/MyAge.java
@@ -0,0 +1,20 @@
+public class MyAge {
+  
+  public static void main(String[] args) {
+    {
+      {
+        try {
+          if (args.length >= 1) {
+            System.out.print("You are ");
+            System.out.print(args[0]);
+            System.out.println(" years old.");
+          }
+          System.out.printf("My AM is %s", args[1]);
+          }
+        catch (Exception e){
+          System.out.println("Den iparxoun arketa orismata");
+        }
+      }
+    }
+  }
+}
